43792960a860bed3727174d545de907fc59871c7
kadoryFT <72969287+kadoryFT@users.noreply.github.com>
2023-06-04 00:29:38 +0300

    last1

---
+++ index.js
@@ -0,0 +1,30 @@
+
+
+
+import inquirer from 'inquirer';
+import qr  from"qr-image";
+import fs, { writeFile } from "fs";
+
+inquirer
+  .prompt([{
+   message: "type your website",
+   name: "URL"}])
+  .then((answers) => {
+    const url =answers.URL
+    var qr_svg = qr.image( url );
+    qr_svg.pipe(fs.createWriteStream("qr_img.png"));
+
+    fs.writeFile("message.txt", url, (err) =>{
+        if (err) throw err;
+        console.log("The file has been saved!");
+    });
+
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
+
